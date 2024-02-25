da95c973169b6f2af30c8f82db7021e0666fe380
Jakub Stelmasiak <stelmon3@gmail.com>
2024-02-25 11:22:19 +0100

    card bug

--- src/components/features/Posts.js
+++ src/components/features/Posts.js
@@ -3,13 +3,14 @@ import { getAllPosts } from "../../redux/postsRedux";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import style from "./Posts.module.scss";
 
 const Posts = () => {
     const posts = useSelector(getAllPosts);
     return (
-        <div className="row">
+        <div className="row d-flex flex-wrap">
             {posts.map(post => (
-                <Card className="rounded mx-2 mt-3 col-sm-12 col-md p-0">
+                <Card className={`rounded mt-3 col-sm-12 col-md p-0 ${style.around}`}>
                 <aritcle className="m-3" to={`/post/${post.id}`} key={post.id}>
                     <h5>{post.title}</h5>
                     <span><strong>Author: </strong></span>{post.author}<br />
